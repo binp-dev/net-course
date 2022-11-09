16d310d383a5074ba594e40441bc0a10c6bb35c2
Alexey Gerasev <alexey.gerasev@gmail.com>
2022-11-09 12:37:08 +0700

    Bad proxy is bidirectional now

--- examples/udp/bad-proxy.py
+++ examples/udp/bad-proxy.py
@@ -17,27 +17,47 @@ def poisson(rng: Random, p: float, n: int) -> int:
 async def main():
     rng = Random(seed)
 
-    dst = ("192.168.1.2", 7777)  # put your address here
+    client_addr = None
+    server_addr = ("192.168.1.2", 7777)  # put your address here
     sock = await create_socket(local_addr=("0.0.0.0", 9999))
     try:
         while True:
             data, src = await sock.recvfrom()
             print(f"{src}: {data}")
+            if src != server_addr:
+                assert client_addr is None or client_addr == src
+                client_addr = src
+                dst = server_addr
+            else:
+                assert client_addr is not None
+                dst = client_addr
 
             # drop
             if rng.uniform(0.0, 1.0) > drop_p:
                 # repeat
                 for _ in range(poisson(rng, repeat_p, repeat_n - 1) + 1):
                     # postpone
-                    async def send_later(data: bytes, delay: float):
+                    async def send_later(data: bytes, addr, delay: float):
                         await asyncio.sleep(delay)
-                        sock.sendto(data, addr=dst)
+                        sock.sendto(data, addr=addr)
 
                     asyncio.create_task(
-                        send_later(copy(data), rng.expovariate(1.0 / postpone_s)),
+                        send_later(
+                            copy(data),
+                            copy(dst),
+                            rng.expovariate(1.0 / postpone_s),
+                        ),
                     )
     finally:
         sock.close()
 
 
 asyncio.run(main())
+
+"""
+Run test server:
+nc -u -s 0.0.0.0 -p 7777 <your-ip-addr> 9999
+
+Run test client:
+nc -u <your-ip-addr> 9999
+"""
